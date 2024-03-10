a11a33d079cd3831f987142b80a1b3f5ac0a20b2
lalinpethiyagoda <lpethiyagoda@alpian.com>
2024-03-10 16:32:38 +0000

    PrototypeDemo.java

--- src/main/java/com/java/stuff/random/functional/FunctionRunner.java
+++ src/main/java/com/java/stuff/random/functional/FunctionRunner.java
@@ -1,5 +1,8 @@
 package com.java.stuff.random.functional;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 import lombok.var;
 
 public class FunctionRunner {
@@ -8,9 +11,18 @@ public class FunctionRunner {
   CalculateSum sumFunction = (x, y) -> x + y;
   var val = returnSum(sumFunction); // lambda allows to create the implementation for the method
     System.out.println(val);
+    BiFunction<Integer, Integer, Integer> multiply = (x , y) -> x * y;
+    var m = returnMultiply(3, 2 , multiply);
+    System.out.println(m);
+    System.out.println(subtract(3, v -> v -1));
   }
-
   public static int returnSum(CalculateSum sum){ // we passed the lambda impl
     return sum.calculateSum(2,3); //Call the method
+  }
+  public static int returnMultiply(int x, int y ,  BiFunction<Integer, Integer, Integer> multiplier) {
+    return multiplier.apply(x,y);
+  }
+  public static int subtract(int x, Function<Integer, Integer> subtract) {
+    return subtract.apply(x);
   }
 }
